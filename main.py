b5129b17745b2786907f0e02edd0fa857f6bf85d
foerfelixir <foerfelixir@gmail.com>
2025-01-15 16:58:22 +0800

    added word counts function.

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ def read_books(path_to_file):
 
     return file_contents
 
+def return_word_counts(book_content) :
+    count = 0
+    words = book_content.split()
+    for whole_word in words:
+        count += 1
+
+    return count
 
 def main():
     status = 0
-    print(read_books("books/frankenstein.txt"))
+    path_to_book = "books/frankenstein.txt"
+    print(read_books(path_to_book))
+    print(f"total word count :{return_word_counts(read_books(path_to_book))}")
     return status
 
 main()
