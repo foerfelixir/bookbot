ed221109afff7dbeba8b1379773e39ec052651f8
foerfelixir <foerfelixir@gmail.com>
2025-01-15 16:38:46 +0800

    Changing main.py to read the content of frankenstein.txt located in books folder, and print it out to the console.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+def read_books(path_to_file):
+    file_contents = ""
+    with open(path_to_file) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+def main():
+    status = 0
+    print(read_books("books/frankenstein.txt"))
+    return status
+
+main()
